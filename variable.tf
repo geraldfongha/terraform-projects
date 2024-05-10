3e31c3783fc83edb4ed25a58dbe4827b5c0949c2
gerald fongha <fitzgeraldh2k@gmail.com>
2024-05-10 23:21:37 +0100

    created security group file

--- variable.tf
+++ variable.tf
@@ -40,3 +40,11 @@ variable "public_subnet_az2_cidr" {
     description  = "private data subnet az2 cidr block"
     type         = string
   }
+
+  #security group variable
+     variable "ssh_location" {
+    default      = "0.0.0.0/0"
+    description  = "the ip address that can ssh into the ec2"
+    type         = string
+  }
+  
